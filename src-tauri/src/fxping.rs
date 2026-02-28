78bf88c096e598ca28b54af1a0ae0947703b2b59
NAKAOKU Takahiro <giraffe_rakugaki@yahoo.co.jp>
2026-02-28 11:45:12 +0900

    ビルドサイズを縮小

--- src-tauri/src/fxping.rs
+++ src-tauri/src/fxping.rs
@@ -143,7 +143,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             ping_target,
             traceroute_target,
